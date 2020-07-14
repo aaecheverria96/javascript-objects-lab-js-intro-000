351a3996dba091e585cd9b5540304a4f4e644da1
Andrew Echeverria <andrew.a.echeverria@gmail.com>
2020-07-14 19:23:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,13 @@ function destructivelyUpdateObjectWithKeyAndValue (object,key,value) {
   object[key] = value
   return object
 }
+
+function deleteFromObjectByKey (object,key,value) { 
+  var object = {foo:'bar'}
+  var newobject = Object.assign({}, obj) 
+
+  delete newObject.foo 
+
+  newObject 
+  Object 
+}
